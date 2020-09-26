06577e635f2f978df5650fa4406300fa9b583803
fogjama <66557609+fogjama@users.noreply.github.com>
2020-09-26 13:45:25 +0100

    Animate enemy

--- main.py
+++ main.py
@@ -31,20 +31,23 @@ class Player(pygame.sprite.Sprite):
     Spawn a player
     '''
 
-    def __init__(self):
+    def __init__(self, x, y, p_img):
         pygame.sprite.Sprite.__init__(self)
         self.movex = 0  # move along x
         self.movey = 0  # move along y
         self.frame = 0  # count frames
+        self.health = 10 # track player health
         self.images = []
 
         for i in range(0,8):
-            img = pygame.image.load(os.path.join('assets/supertux', f'walk-{i}.png')).convert()
+            img = pygame.image.load(os.path.join('assets/supertux', f'{p_img}-{i}.png')).convert()
             img.convert_alpha()     # optimize alpha
             img.set_colorkey(ALPHA) # set alpha
             self.images.append(img)
         self.image = self.images[0]
         self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
     
     def control(self, x, y):
         '''
@@ -54,6 +57,12 @@ class Player(pygame.sprite.Sprite):
         self.movey += y
     
     def update(self):
+        
+        hit_list = pygame.sprite.spritecollide(self, enemy_list, False)
+        for enemy in hit_list:
+            self.health -= 1
+            print(self.health)
+
         '''
         Update sprite position
         '''
@@ -81,8 +90,8 @@ class Enemy(pygame.sprite.Sprite):
     '''
     def __init__(self, x, y, img_file):
         pygame.sprite.Sprite.__init__(self)
-        self.movex = 0
-        self.movey = 0
+        # self.movex = 0
+        # self.movey = 0
         self.frame = 0
         self.images = []
 
@@ -95,28 +104,43 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.counter = 0
     
-    def move(self, x, y):
-        self.movex += x
-        self.movey += y
-    
-    def update(self):
-        self.rect.x += self.movex
-        self.rect.y += self.movey
+    def move(self):
+        # self.movex += x
+        # self.movey += y
 
-        # Moving left
-        if self.movex < 0:
-            self.frame += 1
-            if self.frame > 3*ani:
-                self.frame = 0
-            self.image = pygame.transform.flip(self.images[self.frame//ani], True, False)
-        
-        # Moving right
-        if self.movex > 0:
+        distance = 60
+        speed = 3
+
+        if self.counter >= 0 and self.counter <= distance:
+            self.rect.x += speed
             self.frame += 1
             if self.frame > 3*ani:
                 self.frame = 0
             self.image = self.images[self.frame//ani]
+        elif self.counter >= distance and self.counter <= distance*2: 
+            self.rect.x -= speed
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = pygame.transform.flip(self.images[self.frame//ani], True, False)
+        else:
+            self.counter = 0
+
+        self.counter += 1
+
+
+class Level():
+    def bad(lvl, eloc):
+        if lvl == 1:
+            enemy = Enemy(eloc[0],eloc[1],'bug')
+            enemy_list = pygame.sprite.Group()
+            enemy_list.add(enemy)
+        if lvl == 2:
+            print('Level ' + str(lvl))
+
+        return enemy_list
 
 '''
 Setup
@@ -129,15 +153,18 @@ world = pygame.display.set_mode([worldx,worldy])
 backdrop = pygame.image.load(os.path.join('assets/kenny/Backgrounds','backgroundColorDesert.png'))
 backdropbox = world.get_rect()
 
-player = Player()   # Spawn player
-player.rect.x = 0   # Go to x
-player.rect.y = 0   # Go to y
+player = Player(0,30,'walk')   # Spawn player
+# player.rect.x = 0   # Go to x
+# player.rect.y = 0   # Go to y
 player_list = pygame.sprite.Group()
 player_list.add(player)
 
-bug = Enemy(random.randint(100,500),0,'bug')
-enemy_list = pygame.sprite.Group()
-enemy_list.add(bug)
+eloc = []
+eloc = [random.randint(100,500),0]
+enemy_list = Level.bad(1, eloc)
+# bug = Enemy(random.randint(100,500),0,'bug')
+# enemy_list = pygame.sprite.Group()
+# enemy_list.add(bug)
 
 steps = 10  # how many pixels to move
 
@@ -189,6 +216,8 @@ while main == True:
     player_list.draw(world) # draw player
     # bug.update()
     enemy_list.draw(world)
+    for e in enemy_list:
+        e.move()
 
     pygame.display.flip()
     clock.tick(fps)
